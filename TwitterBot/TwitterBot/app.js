e197f6cae7e857a229d0555e484b8647340ae2e3
SPMNJ <smcguirenj@gmail.com>
2017-04-29 14:33:45 -0400

    Test

--- TwitterBot/TwitterBot/app.js
+++ TwitterBot/TwitterBot/app.js
@@ -1,6 +1,10 @@
 'use strict';
 var Twit = require('twit');
-
+var token = require('./token.js');
 var t = new Twit({
-    consumer_key: 
-}
+    consumer_key: token.consumer_key,
+    consumer_secret: token.consumer_secret,
+    access_token: token.access_token,
+    access_token_secret: token.access_token_secret
+});
+
