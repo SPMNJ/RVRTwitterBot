0eff85a96b0e29d2f73c6d0d0df738d13a11f011
SPMNJ <smcguirenj@gmail.com>
2017-04-29 14:38:48 -0400

    Testing Twitter API

--- TwitterBot/TwitterBot/app.js
+++ TwitterBot/TwitterBot/app.js
@@ -7,4 +7,14 @@ var t = new Twit({
     access_token: token.access_token,
     access_token_secret: token.access_token_secret
 });
+var stream = t.stream('user');
 
+stream.on('direct_message', function (eventMsg) {
+    var msg = eventMsg.direct_message.text;
+    var screenName = eventMsg.direct_message.sender.screen_name;
+    var msgID = eventMsg.direct_message.id_str;
+
+    console.log('I just received a message from ' + screenName);
+    console.log('msg: ' + msg);
+    console.log('id: ' + msgID);
+});
